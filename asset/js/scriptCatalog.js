359e347a5260e45531e54335c96337506749c42d
Viieyeon <98655811+Viieyeon@users.noreply.github.com>
2023-05-29 01:43:21 +0300

    detectOverflow

---
+++ asset/js/scriptCatalog.js
@@ -0,0 +1,379 @@
+
+var cardsData = [
+  {
+
+    imgLike: './asset/img/like.svg',
+    imgUrl: './asset/img/5.png',
+    text: 'Кордилина',
+    price: '3000₽',
+    imgBas: './asset/img/bas.svg',
+    flowerHeight: '40см',
+    utility: 'улучшает качество воздуха',
+    relationLight: 'яркий, но рассеянный свет',
+    watering: 'регулярное увлажнение почвы',
+    lighting: 'без прямого солнечного света',
+    infoOne: 'Кордилина вырастает до 1-2 метров в высоту в зависимости от условий содержания. У кордилины листья находятся на вершинах стволов, образуя куст. Листья могут иметь различную форму, от полосатых до ленточных. Листья кордилины часто окрашены в яркие цвета, такие как красный, розовый, желтый или оранжевый.',
+    infoTwo: 'Кордилина - это неприхотливое растение, которое может выжить в различных условиях. Оно предпочитает яркий свет, но может выжить и в частичной тени. Идеальная температура для кордилины - от 18 до 27 градусов Цельсия. Однако она может переносить и низкие температуры до 10 градусов Цельсия. Растение требует регулярного полива, но без застоя воды в горшке.',
+    infoThree: 'Кордилина имеет множество лечебных свойств. Она содержит антиоксиданты, которые защищают клетки от свободных радикалов. Она также является источником витаминов и минералов, таких как витамин С, калий и кальций. '
+  },
+  {
+    imgLike: './asset/img/like.svg',
+    imgUrl: './asset/img/6.png',
+    text: 'Фикус',
+    price: '3450₽',
+    imgBas: './asset/img/bas.svg',
+    flowerHeight: '30см',
+    utility: 'очищает воздух, увеличивает уровень кислорода',
+    relationLight: 'яркий, но не прямой свет',
+    watering: 'регулярно, но не избыточно',
+    lighting: 'от люминесцентных до натурального солнечного света.',
+    infoOne: ' В домашних условиях чаще всего выращивают фикус бенджамина – это вид, который при правильном уходе может достигать высоты до 1,5 метра. Для фикуса характерен паразитизм – некоторые виды растения используют деревья-хозяева для поддержания своей жизнедеятельности. Растения фикуса могут использоваться в ландшафтном дизайне – для озеленения уличных и внутренних пространств.',
+    infoTwo: 'Фикусы предпочитают яркий разреженный солнечный свет, но терпят и небольшой полутень. Растение неприхотливо в уходе, однако не выносит резкого изменения условий – пересадка, изменение температуры, освещения и прочее могут негативно сказаться на здоровье растения.',
+    infoThree: 'Во многих странах фикус считается символом богатства и процветания.'
+  },
+  {
+    imgLike: './asset/img/like.svg',
+    imgUrl: './asset/img/7.png',
+    text: 'Калатея',
+    price: '2000₽',
+    imgBas: './asset/img/bas.svg',
+    flowerHeight: '45см',
+    utility: 'удаляет из воздуха токсины и вредные химические вещества',
+    relationLight: 'яркий, но рассеянный свет',
+    watering: 'земля должна быть всегда слегка влажной',
+    lighting: 'в углу комнаты, где есть достаточно света',
+    infoOne: ' Калатея - это красивое и популярное комнатное растение, которое известно своими яркими и красочными листьями. Это растение родом из тропических лесов Южной Америки и имеет множество преимуществ для жизни в домашних условиях.',
+    infoTwo: 'Что касается освещения, Калатея требует яркого, но рассеянного света. Она не должна быть выставлена на прямые солнечные лучи, так как это может привести к ожогам листьев. Лучше всего поставить ее в углу комнаты, где есть достаточно света, но прямые солнечные лучи не попадают на нее. Полив Калатеи также требует особого внимания. Она любит влажность, и ее земля должна быть всегда слегка влажной. Однако, ее также не стоит перезаливать водой, так как это может привести к гниению корней. Лучше всего поливать ее один-два раза в неделю, в зависимости от уровня влажности в помещении.',
+    infoThree: 'Это растение родом из тропических лесов Южной Америки и имеет множество преимуществ для жизни в домашних условиях'
+  },
+  {
+    imgLike: './asset/img/like.svg',
+    imgUrl: './asset/img/9.png',
+    text: 'Аглаонема',
+    price: '4300₽',
+    imgBas: './asset/img/bas.svg',
+    flowerHeight: '55см',
+    utility: 'увлажняет воздух и улучшает его качество',
+    relationLight: 'теневыносливое',
+    watering: 'поливать ее землю небольшими порциями, но часто',
+    lighting: 'предпочитает небольшое количество света',
+    infoOne: 'Аглаонема – это красивое, зеленое растение с длинными, узкими листьями. Его листья бывают различных размеров и форм, но все они имеют своеобразный желтым цветком.',
+    infoTwo: 'Аглаонема может расти в различных условиях, но она лучше всего себя чувствует при температуре от 18 до 28 градусов Цельсия и в условиях с относительной влажностью воздуха от 50 до 60 процентов.',
+    infoThree: 'Аглаонема была выведена из тропических лесов Юго-Восточной Азии и иногда называется "китайской долей". Она является одной из самых часто встречающихся растений в офисах, школах и других закрытых помещениях благодаря своей способности очищать воздух.'
+  },
+  {
+    imgLike: './asset/img/like.svg',
+    imgUrl: './asset/img/10.png',
+    text: 'Монстера',
+    price: '6500₽',
+    imgBas: './asset/img/bas.svg',
+    flowerHeight: '43см',
+    utility: 'очищает воздух, увеличивает уровень кислорода',
+    relationLight: ' предпочитает яркий, но разборчивый свет',
+    watering: 'регулярно, но не избыточно',
+    lighting: 'на восточном или западном окне.',
+    infoOne: ' Монстера – это деревообразное растение, которое имеет большие листья с пышными разрезами. Внутри листьев находятся белые пятна и яркие желтые пятнышки на внешних сторонах. Монстера является декоративным растением, которое улучшает качество воздуха в помещении путем очистки его от вредных веществ.',
+    infoTwo: ' Монстера нуждается в тепле и влажности, поэтому рекомендуется держать растение в помещении при температуре не ниже 18°C. Хорошая вентиляция и высокая влажность помогают растению расти лучше.',
+    infoThree: 'Монстера получила свое название из-за гигантских листьев, которые могут достигать длины более 90 см. В природе это растение может достигать высоты более 15 метров. Кроме того, Монстера является частью культуры индейцев Америки, которые используют ее для защиты от злых духов.'
+  },
+  {
+    imgLike: './asset/img/like.svg',
+    imgUrl: './asset/img/11.png',
+    text: 'Филодендрон',
+    price: '5200₽',
+    imgBas: './asset/img/bas.svg',
+    flowerHeight: '65см',
+    utility: 'очищает воздух, увеличивает уровень кислорода',
+    relationLight: 'яркий, но рассеянный свет',
+    watering: 'земля не должна пересыхать, но и не должна быть сильно мокрой.',
+    lighting: 'должно быть светлое, но защищено от прямых лучей солнца.',
+    infoOne: ' Филодендрон - вечнозеленое растение с крупными, глянцевыми и сердцевидными листьями. Растет быстро и достигает больших размеров.',
+    infoTwo: 'Филодендрону необходима высокая влажность воздуха, тепло (18-24°C) и постоянный доступ к свету.',
+    infoThree: 'Название Филодендрон происходит от греческих слов "φίλος" (друг) и "δένδρον" (дерево), что означает "дерево-друг". Некоторые виды Филодендрона обладают антибактериальными свойствами и используются в народной медицине для лечения ран, ожогов и других проблем с кожей.'
+  },
+  {
+    imgLike: './asset/img/like.svg',
+    imgUrl: './asset/img/12.png',
+    text: 'Эпипремнум',
+    price: '5540₽',
+    imgBas: './asset/img/bas.svg',
+    flowerHeight: '40см',
+    utility: 'поглощает избыток углекислого газа',
+    relationLight: 'яркий рассеянный свет, но может расти и в тени.',
+    watering: 'орошение сверху, чтобы не переувлажнить корни.',
+    lighting: 'от люминесцентных до натурального солнечного света.',
+    infoOne: ' Это растение с длинными, зелеными, сердцевидными листьями, которые по мере роста могут достигать до 90 см в длину. Оно принадлежит к семейству ароидных растений и известно своей простотой ухода.',
+    infoTwo: 'Эпипремнум растет в субтропических и тропических регионах, где встречается в влажных лесах. Он хорошо растет в помещении при температуре воздуха от 18 до 25 °С.',
+    infoThree: 'В природе этот вид может достигать длины до 30 метров и обвиваться вокруг других деревьев. Некоторым людям он может вызывать аллергические реакции, поэтому при контакте с растением необходимо быть осторожным.'
+  },
+  {
+    imgLike: './asset/img/like.svg',
+    imgUrl: './asset/img/13.png',
+    text: 'Спатифиллум',
+    price: '5430₽',
+    imgBas: './asset/img/bas.svg',
+    flowerHeight: '30см',
+    utility: 'очищает воздух',
+    relationLight: 'предпочитает полутень или тень и не переносит прямые солнечные лучи.',
+    watering: 'поливать когда верхний слой почвы подсохнет на 1-2 см',
+    lighting: 'в прохладном и тенистом уголке комнаты.',
+    infoOne: 'Спатифиллум - это вечнозеленое растение, которое отличается крупными и блестящими темно-зелеными листьями и мелкими белыми или розовыми цветками. Растение может достигать высоты до 90 см.',
+    infoTwo: 'Спатифиллум предпочитает сырую почву, но не переносит переувлажнение. Также растение чувствительно к перепадам температур.',
+    infoThree: 'Спатифиллум был впервые открыт в 1870 году, а в переводе с греческого его название означает "широколистный". Также Спатифиллум может использоваться в качестве символа материнства и женственности.'
+  },
+  {
+    imgLike: './asset/img/like.svg',
+    imgUrl: './asset/img/14.png',
+    text: 'Замиокулькас',
+    price: '7650₽',
+    imgBas: './asset/img/bas.svg',
+    flowerHeight: '50см',
+    utility: 'способствует увлажнению воздуха',
+    relationLight: 'яркий свет, но не переносит прямых солнечных лучей.',
+    watering: 'нуждается в частом поливе',
+    lighting: 'на южном окне.',
+    infoOne: ' Замиокулькас - декоративное растение с толстым стеблем и зелеными листьями. Оно может вырастать более 1 метра в высоту.  Очищает воздух от формальдегида, бензола и толуола и способствует увлажнению воздуха. Замиокулькас любит яркий свет, но не переносит прямых солнечных лучей.',
+    infoTwo: 'Растение легко переносит засуху и не требует особого ухода.',
+    infoThree: 'Замиокулькас в домашних условиях может жить более 10 лет. В африканской мифологии растение считалось символом богатства и процветания.'
+  }
+];
+
+//генерация карточек
+
+let makeElement = function (tagName, className, text) {
+  let element = document.createElement(tagName);
+  element.classList.add(className);
+  if (text) {
+    element.textContent = text;
+  }
+  return element;
+}
+
+
+let renderCards = function (cards) {
+  let listItem = makeElement('li', 'card');
+  let articleItem = makeElement('article', 'product');
+
+  let pictureLike = makeElement('img', 'card__like');
+  pictureLike.src = cards.imgLike;
+  pictureLike.alt = 'like';
+  articleItem.appendChild(pictureLike);
+
+
+
+  let picture = makeElement('img', 'card-img-top');
+  picture.src = cards.imgUrl;
+  picture.alt = cards.text;
+  articleItem.appendChild(picture);
+
+
+  let linkTitle = makeElement('a', 'title-link');
+  let cardBody = makeElement('div', 'card-body');
+  let title = makeElement('h3', 'card-title', cards.text);
+  title.classList.add('uppercase');
+  title.classList.add('ta-c');
+  linkTitle.appendChild(title);
+  cardBody.appendChild(linkTitle);
+
+  let infoBody = makeElement('div', 'd-flex');
+  infoBody.classList.add('justify-content-between');
+  infoBody.classList.add('align-items-center');
+
+  let infoPrice = makeElement('span', 'card__price', cards.price);
+  infoBody.appendChild(infoPrice);
+
+  let linkBas = makeElement('button', 'bas-link');
+  let pictureBas = makeElement('img', 'card__base');
+  pictureBas.src = cards.imgBas;
+  pictureBas.alt = 'bas';
+  linkBas.appendChild(pictureBas);
+
+  infoBody.appendChild(linkBas);
+  cardBody.appendChild(infoBody);
+  articleItem.appendChild(cardBody);
+  listItem.appendChild(articleItem);
+
+
+
+  return listItem;
+}
+
+let list = document.querySelector('.catologPage__list');
+for (let i = 0; i < cardsData.length; i++) {
+  let cardItem = renderCards(cardsData[i]);
+  list.appendChild(cardItem);
+}
+
+
+//корзина товаров 
+
+const productsBtn = document.querySelectorAll('.bas-link');
+const cartProductsList = document.querySelector('.cart-content__list');
+const cart = document.querySelector('.cart');
+const cartQuantity = document.querySelector('.card__quantity');
+const fullPrice = document.querySelector('.fullprice');
+let price = 0;
+
+const randomId = () => {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+};
+
+const priceWithoutSpaces = (str) => {
+  return str.replace(/\s/g, '');
+};
+
+const normalPrice = (str) => {
+  return String(str).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+};
+
+const generateCardProduct = (img, title, price, id) => {
+  return `
+  <li class="cart-content__item">
+  <article class="cart-content__product product data-id="${id}"">
+      <img src="${img}" alt="" class="cart-product__img">
+      <div class="cart-product__text">
+          <h3 class="cart-product__title uppercase">${title}</h3>
+          <span class="cart-product__price">${normalPrice(price)}₽</span>
+      </div>
+      <button class="cart-product__delete" aria-label="Удалить товар"></button>
+  </article>
+</li>
+  
+  `
+}
+
+const plusFullPrice = (curentPrice) => {
+  return price += curentPrice;
+};
+
+const minusFullPrice = (curentPrice) => {
+  return price -= curentPrice;
+};
+
+const printFullPrice = () => {
+  fullPrice.textContent = `${normalPrice(price)}₽ `
+};
+
+const printQuantity = () => {
+  let length = cartProductsList.querySelector('.simplebar-content').children.length;
+  cartQuantity.textContent = length;
+  length > 0 ? cart.classList.add('active') : cart.classList.remove('active');
+}
+
+const deleteProducts = (productParent) => {
+  let id = productParent.querySelector('.product').dataset.id;
+
+  let curentPrice = parseInt(priceWithoutSpaces(productParent.querySelector('.cart-product__price').textContent));
+  minusFullPrice(curentPrice);
+  printFullPrice();
+  productParent.remove();
+  printQuantity();
+}
+
+productsBtn.forEach(el => {
+  el.closest('.product').setAttribute('data-id', randomId());
+  el.addEventListener('click', (e) => {
+    let self = e.currentTarget;
+    let parent = self.closest('.product');
+    let id = parent.dataset.id;
+    let img = parent.querySelector('.card-img-top').getAttribute('src');
+    let title = parent.querySelector('.card-title').textContent;
+    let priceNumber = parseInt(priceWithoutSpaces(parent.querySelector('.card__price').textContent));
+
+    plusFullPrice(priceNumber);
+    printFullPrice();
+    cartProductsList.querySelector('.simplebar-content').insertAdjacentHTML('afterbegin', generateCardProduct(img, title, priceNumber, id));
+    printQuantity();
+  });
+});
+
+cartProductsList.addEventListener('click', (e) => {
+  if (e.target.classList.contains('cart-product__delete')) {
+    deleteProducts(e.target.closest('.cart-content__item'));
+  }
+})
+
+
+//генерация информации
+const catologPageInfo = document.querySelector('.catologPage__info');
+const catalogArticle = document.querySelectorAll('.product');
+
+const assignId = () => {
+  for (let i = 0; i < cardsData.length; i++) {
+    cardsData[i].id = catalogArticle[i].dataset.id;
+  }
+}
+
+const generateCardInfo = (cards) => {
+  return `
+  <div class="card-info">
+      <div class="d-flex card-info-inner">
+          <img src="${cards.imgUrl}" alt="${cards.text}" class="info-img">
+          <div class="info-text d-flex flex-column">
+              <h3 class="info-title uppercase">
+                ${cards.text}
+              </h3>
+              <span class="info-price">
+                ${cards.price}
+              </span>
+              <span class="characteristic">
+                  Высота растения: ${cards.flowerHeight}
+              </span>
+              <div class="flower-person-info d-flex flex-column">
+                  <span class="utility text-normal">
+                      <span class='characteristic'>Полезность:</span> ${cards.utility}
+                  </span>
+                  <span class="relationLight text-normal">
+                      <span class='characteristic'>Отношение к свету:</span> ${cards.relationLight}
+                  </span>
+                  <span class="watering text-normal">
+                      <span class='characteristic'>Полив:</span> ${cards.watering}
+                  </span>
+                  <span class="lighting text-normal">
+                      <span class='characteristic'>Освещение:</span> ${cards.lighting}
+                  </span>
+              </div>
+          </div>
+          <button class="cart-product__delete" aria-label="Удалить товар"></button>
+      </div>
+      <div class="info-text">
+          <h4 class="characteristic">Комнатное растение ${cards.text}
+          </h4>
+          <p>${cards.infoOne}</p>
+          <h4 class="characteristic">Условия</h4>
+          <p>${cards.infoTwo}</p>
+          <h4 class="characteristic">Интересно</h4>
+          <p>${cards.infoThree}</p>
+      </div>
+  </div> 
+  
+  `
+}
+
+const closingPage = () => {
+  let closing = document.querySelector('.cart-product__delete');
+  closing.addEventListener('click', (e) => {
+    catologPageInfo.classList.remove('active');
+    catologPageInfo.querySelector('.simplebar-content').innerHTML = '';
+  })
+}
+
+document.addEventListener('click', (e) => {
+  let productTitle = e.target;
+  assignId();
+  if (productTitle.classList.contains('card-title') || productTitle.classList.contains('card-img-top')) {
+    let productCard = productTitle.closest('.product');
+    let productId = productCard.dataset.id;
+
+    for (let i = 0; i < cardsData.length; i++) {
+      if (productId === cardsData[i].id) {
+        catologPageInfo.classList.add('active');
+        catologPageInfo.querySelector('.simplebar-content').insertAdjacentHTML('afterbegin', generateCardInfo(cardsData[i]));
+        closingPage();
+      }
+    }
+  }
+})
+
+
+
